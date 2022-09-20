31e90c841c5f7543392eed268ee149a578354a69
Justin <jnstendara@gmail.com>
2022-09-20 10:31:28 -0500

    Trying to add functionality to news article page

--- src/components/newsList.jsx
+++ src/components/newsList.jsx
@@ -4,9 +4,18 @@ var React = require('react');
 var NewsList = (props) => (
   <section className="col-2-3 news">
     <h3>News</h3>
+    <div className = "news-searchbar">
+      <input type='text' placeholder='Search here...' className='news-query'></input><br/>
+      <button>Update news topics</button>
+    </div>
     {props.news.articles.map((article, idx) =>
       <NewsEntry key = {idx} id = {idx} article = {article}/>
     )}
+
+    <div className = 'news-searchbar'>
+      <input type='text' placeholder ="Enter the number of news articles you'd like to see" className='news-quantity' ></input><br/>
+      <button>Update number</button>
+    </div>
   </section>
 )
 
